bb8753b4dc8b3934f8b148435ab9f2be5d110c99
miltonlim1993-rgb <miltonlim1993@gmail.com>
2026-07-26 06:27:26 +0800

    Refresh installed PWA shell after the scroll fix

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'chefops-v4-5-1-release-cf-android-1'
+const VERSION = 'chefops-v4-5-1-release-cf-android-2-scroll-fix'
 const SHELL_CACHE = `${VERSION}-shell`
 const DATA_CACHE = `${VERSION}-data`
 const OCR_CACHE = `${VERSION}-ocr`
