0dad6a6278b6e6203ffd3a724425ce92aeb50265
miltonlim1993-rgb <miltonlim1993@gmail.com>
2026-07-26 07:13:45 +0800

    Load the installed-app viewport lock after the main stylesheet

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
+import '@/viewport.css'
 import '@/lib/install-prompt'
 import { applyTheme } from '@/lib/theme'
 
