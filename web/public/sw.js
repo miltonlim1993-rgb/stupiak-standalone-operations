4b30413c9421d065e82553285b95695baead77c5
miltonlim1993-rgb <miltonlim1993@gmail.com>
2026-07-26 07:18:15 +0800

    Refresh installed PWA after the bright fixed-shell update

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'chefops-v4-5-1-release-cf-android-3-light-icon-fixed-shell'
+const VERSION = 'chefops-v4-5-1-release-cf-android-4-bright-fixed-scroll-icon'
 const SHELL_CACHE = `${VERSION}-shell`
 const DATA_CACHE = `${VERSION}-data`
 const OCR_CACHE = `${VERSION}-ocr`
