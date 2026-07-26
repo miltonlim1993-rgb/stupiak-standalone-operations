47b7dca6b90115d8d566f7e33d5f3dd924b39c27
miltonlim1993-rgb <miltonlim1993@gmail.com>
2026-07-26 14:21:38 +0800

    Bump service worker for native print panels v9

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'chefops-v4-5-1-stable-panels-shell-v8'
+const VERSION = 'chefops-v4-5-1-native-print-panels-shell-v9'
 const SHELL_CACHE = `${VERSION}-shell`
 const DATA_CACHE = `${VERSION}-data`
 const OCR_CACHE = `${VERSION}-ocr`
