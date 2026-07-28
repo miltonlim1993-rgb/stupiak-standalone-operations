074e40668d30a01e7c58ee95cf88f5cdf71698c1
miltonlim1993-rgb <miltonlim1993@gmail.com>
2026-07-28 20:49:10 +0800

    Expose practical bilingual task worker revision 4.6.10

--- worker/src/entry-v3.js
+++ worker/src/entry-v3.js
@@ -2,7 +2,7 @@ import app from './entry.js'
 import { errorResponse } from './http.js'
 import { handleTaskWorkflowV5 } from './task-workflow-v5.js'
 
-const WORKER_REVISION = 'practical-chain-tasks-v5-native-tspl-v4.6.9'
+const WORKER_REVISION = 'practical-bilingual-chain-tasks-v5-native-tspl-v4.6.10'
 
 function taskApiHeaders(request, response) {
   const headers = new Headers(response.headers)
