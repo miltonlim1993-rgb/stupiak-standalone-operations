8d2c9e2f2c9dddf05daf576d9d322d2c9834eac5
miltonlim1993-rgb <miltonlim1993@gmail.com>
2026-07-28 21:35:03 +0800

    Expose Master Sheet task refresh hotfix revision 4.6.11

--- worker/src/entry-v3.js
+++ worker/src/entry-v3.js
@@ -2,7 +2,7 @@ import app from './entry.js'
 import { errorResponse } from './http.js'
 import { handleTaskWorkflowV5 } from './task-workflow-v5.js'
 
-const WORKER_REVISION = 'practical-bilingual-chain-tasks-v5-native-tspl-v4.6.10'
+const WORKER_REVISION = 'master-task-refresh-bilingual-v6-native-tspl-v4.6.11'
 
 function taskApiHeaders(request, response) {
   const headers = new Headers(response.headers)
