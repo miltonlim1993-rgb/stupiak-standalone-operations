abad227d607aacee76ba5f40233ce9186b4bcba3
miltonlim1993-rgb <miltonlim1993@gmail.com>
2026-07-28 11:48:49 +0800

    Mark direct print media size fix worker 4.6.6

--- worker/src/entry-v3.js
+++ worker/src/entry-v3.js
@@ -2,7 +2,7 @@ import app from './entry.js'
 import { errorResponse } from './http.js'
 import { handleTaskWorkflowV3 } from './task-workflow-v3.js'
 
-const WORKER_REVISION = 'layout-public-label-settings-v4.6.5'
+const WORKER_REVISION = 'direct-print-media-size-fix-v4.6.6'
 
 function taskApiHeaders(request, response) {
   const headers = new Headers(response.headers)
