4a542ceff43b00b4bc914df680a58058a32ff96e
miltonlim1993-rgb <miltonlim1993@gmail.com>
2026-07-30 07:55:20 +0800

    Refresh PWA cache for rebuilt SOP visuals

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'chefops-v4-5-1-guided-sop-learning-v15'
+const VERSION = 'chefops-v4-5-1-guided-sop-source-visual-v16'
 const SHELL_CACHE = `${VERSION}-shell`
 const DATA_CACHE = `${VERSION}-data`
 const OCR_CACHE = `${VERSION}-ocr`
