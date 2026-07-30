a229a2fb71bc626e3d323702ba4b95c83e9ca222
miltonlim1993-rgb <miltonlim1993@gmail.com>
2026-07-30 21:53:56 +0800

    Refresh compact Training Hub service worker cache

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'chefops-v4-6-27-restored-task-sop-ui-v28'
+const VERSION = 'chefops-v4-6-28-compact-training-hub-v29'
 const SHELL_CACHE = `${VERSION}-shell`
 const DATA_CACHE = `${VERSION}-data`
 const OCR_CACHE = `${VERSION}-ocr`
