ea29b3f1b0b8e4a06c8e5b9acab2c0b71928e6c1
miltonlim1993-rgb <miltonlim1993@gmail.com>
2026-07-30 15:41:31 +0800

    Publish 4.6.23 printer installer freshness headers

--- worker/src/entry-v3.js
+++ worker/src/entry-v3.js
@@ -2,8 +2,8 @@ import app from './entry.js'
 import { errorResponse } from './http.js'
 import { handleTaskWorkflowV5 } from './task-workflow-v5.js'
 
-const WORKER_REVISION = 'windows-queue-kitchen-ip-service-v24-v4.6.22'
-const SHELL_REVISION = '4.6.22-windows-queue-kitchen-ip-service-v24'
+const WORKER_REVISION = 'windows-installer-startup-v25-v4.6.23'
+const SHELL_REVISION = '4.6.23-windows-installer-startup-v25'
 
 function taskApiHeaders(request, response) {
   const headers = new Headers(response.headers)
@@ -40,6 +40,7 @@ function withShellHeaders(request, url, response) {
   const isFreshnessResource = url.pathname === '/sw.js'
     || isNavigation
     || ['/labels', '/labels/settings', '/more'].includes(url.pathname)
+    || url.pathname.startsWith('/print-service/')
 
   if (isFreshnessResource) {
     headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
