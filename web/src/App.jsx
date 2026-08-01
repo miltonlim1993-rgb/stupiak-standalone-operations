d7b89b7bef296f8f2955e823a9e39547142c13b9
miltonlim1993-rgb <miltonlim1993@gmail.com>
2026-08-01 20:11:41 +0800

    style: load refreshed SOP interface styles

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,4 +1,5 @@
 import '@/ops-ui-refresh.css'
+import '@/sop-ui-refresh.css'
 import { lazy, Suspense } from 'react'
 import { Toaster } from '@/components/ui/toaster'
 import { QueryClientProvider } from '@tanstack/react-query'
